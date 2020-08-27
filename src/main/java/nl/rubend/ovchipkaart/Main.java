4fac9814654c3f34fe6d4913eed57fae72f2a580
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-08-27 11:28:36 +0200

    layout volgens opdracht

--- src/main/java/nl/rubend/ovchipkaart/Main.java
+++ src/main/java/nl/rubend/ovchipkaart/Main.java
@@ -8,9 +8,10 @@ public class Main {
 			PreparedStatement statement=Connector.getConn().prepareStatement("select reiziger_id,voorletters,tussenvoegsel,achternaam,geboortedatum from reiziger;");
 			statement.execute();
 			ResultSet set=statement.getResultSet();
+			System.out.println("Alle reizigers:");
 			while(set.next()) {
 				String tussenvoegsel=set.getString(3);
-				System.out.println("#"+set.getInt(1)+": "+set.getString(2)+". "+(tussenvoegsel==null?"":tussenvoegsel+" ")+set.getString(4)+" ("+set.getDate(5)+")");
+				System.out.println("\t#"+set.getInt(1)+": "+set.getString(2)+". "+(tussenvoegsel==null?"":tussenvoegsel+" ")+set.getString(4)+" ("+set.getDate(5)+")");
 			}
 		} catch (SQLException exception) {
 			exception.printStackTrace();
