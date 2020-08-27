0cedd58fe682f801a531f8f771d25ea9c026ea26
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-08-27 10:59:47 +0200

    first commit

---
+++ src/main/java/nl/rubend/ovchipkaart/Connector.java
@@ -0,0 +1,22 @@
+package nl.rubend.ovchipkaart;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Objects;
+import java.util.Properties;
+
+public class Connector {
+	private static Connection conn;
+	static {
+		Properties prop = new Properties();
+		try {
+			prop.load(Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResourceAsStream("database.properties")));
+			conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("user"), prop.getProperty("pass"));
+		} catch (SQLException | IOException throwables) {
+			throwables.printStackTrace();
+		}
+	}
+	public static Connection getConn() {return conn;}
+}
---
+++ src/main/java/nl/rubend/ovchipkaart/Main.java
@@ -0,0 +1,20 @@
+package nl.rubend.ovchipkaart;
+
+import java.sql.*;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			PreparedStatement statement=Connector.getConn().prepareStatement("select reiziger_id,voorletters,tussenvoegsel,achternaam,geboortedatum from reiziger;");
+			statement.execute();
+			ResultSet set=statement.getResultSet();
+			while(set.next()) {
+				String tussenvoegsel=set.getString(3);
+				System.out.println("#"+set.getInt(1)+": "+set.getString(2)+". "+(tussenvoegsel==null?"":tussenvoegsel+" ")+set.getString(4)+" ("+set.getDate(5)+")");
+			}
+		} catch (SQLException exception) {
+			exception.printStackTrace();
+		}
+
+	}
+}
