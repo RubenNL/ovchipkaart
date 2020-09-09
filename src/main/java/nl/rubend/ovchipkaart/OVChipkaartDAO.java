1c82032d1227271c0809ed0bfa67ee17cbb5f248
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-09-09 10:19:09 +0200

    OVChipkaart

---
+++ src/main/java/nl/rubend/ovchipkaart/OVChipkaartDAO.java
@@ -0,0 +1,14 @@
+package nl.rubend.ovchipkaart;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface OVChipkaartDAO {
+	List<OVChipkaart> findByReiziger(Reiziger reiziger) throws SQLException;
+	List<OVChipkaart> findByReizigerId(int id) throws SQLException;
+	boolean save(OVChipkaart ovChipkaart);
+	boolean update(OVChipkaart ovChipkaart);
+	boolean delete(OVChipkaart ovChipkaart);
+	List<OVChipkaart> findAll() throws SQLException;
+}
---
+++ src/main/java/nl/rubend/ovchipkaart/OVChipkaartDAOPsql.java
@@ -0,0 +1,83 @@
+package nl.rubend.ovchipkaart;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OVChipkaartDAOPsql implements OVChipkaartDAO{
+	private Connection conn;
+	public OVChipkaartDAOPsql(Connection conn) {
+		this.conn=conn;
+	};
+	private OVChipkaart setToOVChipkaart(ResultSet set) throws SQLException {
+		return new OVChipkaart(set.getInt("kaart_nummer"), set.getDate("geldig_tot"), set.getInt("klasse"), set.getDouble("saldo"), set.getInt("reiziger_id"));
+	}
+	@Override
+	public List<OVChipkaart> findByReizigerId(int reiziger_id) throws SQLException {
+		List<OVChipkaart> response=new ArrayList<>();
+		PreparedStatement statement= SQLConnector.getConn().prepareStatement("SELECT * FROM adres WHERE reiziger_id=?;");
+		statement.setInt(1,reiziger_id);
+		statement.execute();
+		ResultSet set=statement.getResultSet();
+		while(set.next()) response.add(setToOVChipkaart(set));
+		return response;
+	}
+	@Override
+	public List<OVChipkaart> findByReiziger(Reiziger reiziger) throws SQLException {
+		return findByReizigerId(reiziger.getId());
+	}
+
+	@Override
+	public boolean save(OVChipkaart ovChipkaart) {
+		try {
+			PreparedStatement statement = SQLConnector.getConn().prepareStatement("INSERT INTO ov_chipkaart(kaart_nummer,geldig_tot,klasse,saldo,reiziger_id) VALUES (?,?,?,?,?);");
+			statement.setInt(1, ovChipkaart.getKaart_nummer());
+			statement.setDate(2, (Date) ovChipkaart.getGeldig_tot());
+			statement.setInt(3, ovChipkaart.getKlasse());
+			statement.setDouble(4, ovChipkaart.getSaldo());
+			statement.setInt(5,ovChipkaart.getReiziger_id());
+			statement.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean update(OVChipkaart ovChipkaart) {
+		try {
+			PreparedStatement statement = SQLConnector.getConn().prepareStatement("UPDATE ov_chipkaart SET geldig_tot=?,klasse=?,saldo=?,reiziger_id=? WHERE kaart_nummer=?;");
+			statement.setDate(1, (Date) ovChipkaart.getGeldig_tot());
+			statement.setInt(2, ovChipkaart.getKlasse());
+			statement.setDouble(3, ovChipkaart.getSaldo());
+			statement.setInt(4, ovChipkaart.getReiziger_id());
+			statement.setInt(5, ovChipkaart.getKaart_nummer());
+			statement.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean delete(OVChipkaart ovChipkaart) {
+		try {
+			PreparedStatement statement = SQLConnector.getConn().prepareStatement("DELETE FROM ov_chipkaart WHERE kaart_nummer=?;");
+			statement.setInt(1, ovChipkaart.getKaart_nummer());
+			statement.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+
+	@Override
+	public List<OVChipkaart> findAll() throws SQLException {
+		List<OVChipkaart> response=new ArrayList<>();
+		PreparedStatement statement= SQLConnector.getConn().prepareStatement("SELECT * FROM adres;");
+		statement.execute();
+		ResultSet set=statement.getResultSet();
+		while(set.next()) response.add(setToOVChipkaart(set));
+		return response;
+	}
+}
